b59c7371deb2316176e265f91ce25ec440858bf0
khvitmyhr <kihv001@student.kristiania.no>
2023-12-02 21:15:58 +0100

    Endret navn

--- src/test/java/com/backendprogrammeringexamTest/BackendProgrammeringExamApplicationTests.java
+++ src/test/java/com/backendprogrammeringexamTest/BackendProgrammeringExamApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.backendprogrammeringexam;
+package com.backendprogrammeringexamTest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
