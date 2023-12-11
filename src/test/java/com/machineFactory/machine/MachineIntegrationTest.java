08e241788f814ecfff3fae3ad6f60e5d3a698136
Sanderchriss <sanderchriss@gmail.com>
2023-12-11 14:22:29 +0100

    Struktur i Integration-test

--- src/test/java/com/machineFactory/machine/MachineIntegrationTest.java
+++ src/test/java/com/machineFactory/machine/MachineIntegrationTest.java
@@ -1,4 +1,22 @@
 package com.machineFactory.machine;
 
+import com.machineFactory.Controller.CustomerController;
+import com.machineFactory.Controller.MachineController;
+import com.machineFactory.Repository.CustomerRepo;
+import com.machineFactory.Repository.MachineRepo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
 public class MachineIntegrationTest {
+    @Autowired
+    private MachineController machineController;
+    @Autowired
+    private MachineRepo machineRepo;
+
+    @Test
+    public void testMachine(){}
 }
--- src/test/java/com/machineFactory/subassembly/SubassemblyIntegrationTest.java
+++ src/test/java/com/machineFactory/subassembly/SubassemblyIntegrationTest.java
@@ -1,4 +1,20 @@
 package com.machineFactory.subassembly;
 
+import com.machineFactory.Controller.SubassemblyController;
+import com.machineFactory.Repository.SubassemblyRepo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
 public class SubassemblyIntegrationTest {
+    @Autowired
+    private SubassemblyController subassemblyController;
+    @Autowired
+    private SubassemblyRepo subassemblyRepo;
+
+    @Test
+    public void testSubassembly(){}
 }
