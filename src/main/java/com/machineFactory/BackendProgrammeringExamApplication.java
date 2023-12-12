07283485ddfb3ea6ba2a1f67f04c0c4e73da098f
khvitmyhr <kihv001@student.kristiania.no>
2023-12-12 11:05:08 +0100

    Lagt til customer/address

--- src/main/java/com/machineFactory/BackendProgrammeringExamApplication.java
+++ src/main/java/com/machineFactory/BackendProgrammeringExamApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class BackendProgrammeringExamApplication {
@@ -26,6 +27,8 @@ public class BackendProgrammeringExamApplication {
                 OrderRepo orderRepo,
                 PartRepo partRepo,
                 SubassemblyRepo subassemblyRepo,
+                ArrayList<Address> addresses,
+                ArrayList<Customer> customers,
                 ArrayList<Subassembly> subassemblieListForTractor,
                 ArrayList<Subassembly> subassemblieListForChainSaw,
                 ArrayList<Part> partsForMotor,
@@ -65,6 +68,22 @@ public class BackendProgrammeringExamApplication {
 
                 Machine machine = machineRepo.save(new Machine("Chainsaw", subassemblieListForChainSaw));
                 Machine machine2 = machineRepo.save(new Machine("Traktor", subassemblieListForTractor));
+
+                //Create a customer, and add an address to it
+                Customer customer = customerRepo.save(new Customer("Helga", "helga@gmail.com"));
+                Address address = addressRepo.save(new Address("Løkkaveien 8", "8930", "Bergen"));
+                addresses.add(address);
+                customer.setAddresses(addresses);
+                customerRepo.save(customer);
+
+                //Create an address, and add it to a customer
+                Address address2 = addressRepo.save(new Address("Kyllingveien 3", "9483", "Tromsø"));
+                Customer customer2 = customerRepo.save(new Customer("Johny", "johny@gmail.com"));
+                customers.add(customer2);
+                address2.setCustomers(customers);
+                addressRepo.save(address2);
+
+
 
             };
         }
