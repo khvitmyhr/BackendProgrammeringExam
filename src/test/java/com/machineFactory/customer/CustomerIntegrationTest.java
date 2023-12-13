670cf629545745c510332f3e663ac1f39b8e1049
khvitmyhr <kihv001@student.kristiania.no>
2023-12-13 13:43:31 +0100

    Lagt til tester

---
+++ src/test/java/com/machineFactory/customer/CustomerIntegrationTest.java
@@ -0,0 +1,79 @@
+package com.machineFactory.customer;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.machineFactory.Model.Customer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CustomerIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void shouldGetAllCustomersAndGet200() throws Exception {
+        mockMvc.perform(get("/api/customer"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldReceive404FromCustomerControllerIfWrongURL() throws Exception {
+        mockMvc.perform(get("/api/customers"))
+                .andExpect(status().isNotFound());
+    }
+
+
+    @Test
+    public void shouldDeleteFirstCustomerGet200() throws Exception {
+        mockMvc.perform(delete("/api/customer/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldPostCustomerAndGet200() throws Exception {
+        Customer customer = new Customer("Jens", "jens@gmail.com");
+
+        mockMvc.perform(post("/api/customer")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(customer)))
+                .andExpect(status().isOk());
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+//
+//
+//    @Autowired
+//    private CustomerController customerController;
+//    @Autowired
+//    private CustomerRepo customerRepo;
+//
+//    @Test
+//    public void testCustomer(){
+//        //Customer customer = new Customer("Ivar");
+//        //customerRepo.save(customer);
+//        //Customer Customer = customerController.getCustomerById(customer.getCustomerId());
+//        //assertEquals(HttpStatus.OK, Customer.());
+//        // assertEquals("Ivar", Customer.getCustomerName());
+//    }
+//}
+
+}
+
