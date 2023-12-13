e3e0c98f061a3a83477990f6553e28499e4dfe9b
khvitmyhr <kihv001@student.kristiania.no>
2023-12-13 14:10:15 +0100

    Lagt til machine end to end

---
+++ src/test/java/com/machineFactory/machine/MachineEndToEndTest.java
@@ -0,0 +1,40 @@
+package com.machineFactory.machine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class MachineEndToEndTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void shouldGetOneMachineAndGet200() throws Exception {
+        mockMvc.perform(get("/api/machine/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldReceive404FromMachineControllerIfWrongURL() throws Exception {
+        mockMvc.perform(get("/api/machines"))
+                .andExpect(status().isNotFound());
+    }
+
+
+    @Test
+    public void shouldDeleteFirstMachineGet200() throws Exception {
+        mockMvc.perform(delete("/api/machine/1"))
+                .andExpect(status().isOk());
+    }
+
+}
+
