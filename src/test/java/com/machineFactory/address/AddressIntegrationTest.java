6c769ab283af5b2c2646757e12c932a91efdd29c
khvitmyhr <kihv001@student.kristiania.no>
2023-12-13 13:30:05 +0100

    endret navn

--- src/test/java/com/machineFactory/address/AddressIntegrationTest.java
+++ src/test/java/com/machineFactory/address/AddressIntegrationTest.java
@@ -15,7 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class AddressServiceIntegrationTest {
+public class AddressIntegrationTest {
 
     @Autowired
     MockMvc mockMvc;
