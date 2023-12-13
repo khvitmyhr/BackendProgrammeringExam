37d0e9ab80a37f5d9336ee803829cc05528c68b2
khvitmyhr <kihv001@student.kristiania.no>
2023-12-13 16:24:31 +0100

    Added integrationTest til Part

--- src/test/java/com/machineFactory/part/PartIntegrationTest.java
+++ src/test/java/com/machineFactory/part/PartIntegrationTest.java
@@ -1,20 +1,24 @@
 package com.machineFactory.part;
 
-import com.machineFactory.Controller.PartController;
-import com.machineFactory.Repository.PartRepo;
+import com.machineFactory.Service.PartService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+
 @SpringBootTest
-@Transactional
 public class PartIntegrationTest {
+
     @Autowired
-    private PartController partController;
-    @Autowired
-    private PartRepo partRepo;
+    PartService partService;
 
     @Test
-    public void testPart(){}
+    @Transactional
+    void shouldFetchAllPartsWithPagination(){
+
+        var parts = partService.getAllPartsWithPagination(0);
+        assert parts.size() == 4;
+    }
 }
+
