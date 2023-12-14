110ce6210734ab09a8553ffe664c71de4036e325
khvitmyhr <kihv001@student.kristiania.no>
2023-12-14 11:37:52 +0100

    Unit DataJpa

--- src/test/java/com/machineFactory/machine/MachineUnitTest.java
+++ src/test/java/com/machineFactory/machine/MachineUnitTest.java
@@ -1,4 +1,23 @@
 package com.machineFactory.machine;
 
+import com.machineFactory.Model.Machine;
+import com.machineFactory.Repository.MachineRepo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+@DataJpaTest
 public class MachineUnitTest {
+
+        @Autowired
+        MachineRepo machineRepo;
+        @Test
+        void shouldFetchAllMachines(){
+            List<Machine> machines = machineRepo.findAll();
+            assert machines.size() == 2;
+        }
 }
+
+//Test from Jason repo: https://github.com/jlwcrews2/jpa-demo/blob/master/src/test/java/no/jlwcrews/jpademo/DBUnitTesting.java
