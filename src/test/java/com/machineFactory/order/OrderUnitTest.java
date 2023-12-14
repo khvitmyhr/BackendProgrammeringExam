7422c416c04f266f1e06dddccad49b77e14fbbc2
khvitmyhr <kihv001@student.kristiania.no>
2023-12-14 11:55:01 +0100

    Unit DataJpa

--- src/test/java/com/machineFactory/order/OrderUnitTest.java
+++ src/test/java/com/machineFactory/order/OrderUnitTest.java
@@ -1,4 +1,36 @@
 package com.machineFactory.order;
 
+
+import com.machineFactory.Model.OrderModel;
+import com.machineFactory.Repository.OrderRepo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+@DataJpaTest
 public class OrderUnitTest {
+
+    @Autowired
+    OrderRepo orderRepo;
+
+    @Test
+    void shouldFetchOrders(){
+        List<OrderModel> orders = orderRepo.findAll();
+        assert orders.size() == 1;
+    }
+    @Test
+    void orderCustomerConfirmName(){
+        OrderModel orderModel = orderRepo.findById(1L).get();
+        assert orderModel.getCustomer().getCustomerName().equals("Jens");
+    }
+
+    @Test
+    void orderContains1Machine(){
+        OrderModel orderModel = orderRepo.findById(1L).get();
+        assert orderModel.getMachines().size() == 1;
+    }
 }
+
+//Tests inspired from Jason repo: https://github.com/jlwcrews2/jpa-demo/blob/master/src/test/java/no/jlwcrews/jpademo/DBUnitTesting.java
