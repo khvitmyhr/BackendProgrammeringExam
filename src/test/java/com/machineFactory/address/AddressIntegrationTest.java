01052d451bdc5e6c6b140d0bbeac793dce7320f3
khvitmyhr <kihv001@student.kristiania.no>
2023-12-14 11:22:07 +0100

    Test

--- src/test/java/com/machineFactory/address/AddressIntegrationTest.java
+++ src/test/java/com/machineFactory/address/AddressIntegrationTest.java
@@ -1,4 +1,31 @@
 package com.machineFactory.address;
 
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class AddressIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void shouldFetchAddresses() throws Exception {
+        mockMvc.perform(get("/api/address"))
+                .andExpect(status().isOk())
+                .andDo(result -> {
+                    System.out.println(result.getResponse().getContentAsString());
+                });
+    }
+
 }
+
+//Test from Jason repo: https://github.com/jlwcrews2/vet-clinic/blob/main/src/test/java/no/jlwcrews/vetclinic/appointment/AppointmentIntegrationTest.java
