53b1d0c5d4426874074a767decbafaa8e4439f99
khvitmyhr <kihv001@student.kristiania.no>
2023-12-14 12:00:46 +0100

    Unit DataJpa

--- src/test/java/com/machineFactory/part/PartUnitTest.java
+++ src/test/java/com/machineFactory/part/PartUnitTest.java
@@ -1,4 +1,28 @@
 package com.machineFactory.part;
+import com.machineFactory.Model.Part;
+import com.machineFactory.Repository.PartRepo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
+@DataJpaTest
 public class PartUnitTest {
+
+    @Autowired
+    PartRepo partRepo;
+    @Test
+    void shouldFetchAllPartsExpectsEquals4(){
+        List<Part> pars = partRepo.findAll();
+        assert pars.size() == 4;
+    }
+
+    @Test
+    void confirmNameOnPart(){
+        Part part = partRepo.findById(2L).get();
+        assert part.getPartName().equals("Piston");
+    }
 }
+
+//Test from Jason repo: https://github.com/jlwcrews2/jpa-demo/blob/master/src/test/java/no/jlwcrews/jpademo/DBUnitTesting.java
