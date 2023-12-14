ffb38e471696283adf8c790c1c2d3f456e1eb6b9
khvitmyhr <kihv001@student.kristiania.no>
2023-12-14 13:51:22 +0100

    .

--- src/test/java/com/machineFactory/address/AddressRepoTest.java
+++ src/test/java/com/machineFactory/address/AddressRepoTest.java
@@ -1,7 +1,6 @@
 package com.machineFactory.address;
 
 import com.machineFactory.Repository.AddressRepo;
-import com.machineFactory.Repository.CustomerRepo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
