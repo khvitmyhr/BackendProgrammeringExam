8b1d4d94c59f3682126283c2d3346f995da06180
khvitmyhr <kihv001@student.kristiania.no>
2023-12-15 14:48:51 +0100

    Satt opp envirement for test

--- src/main/java/com/machineFactory/BackendProgrammeringExamApplication.java
+++ src/main/java/com/machineFactory/BackendProgrammeringExamApplication.java
@@ -3,20 +3,26 @@ package com.machineFactory;
 import com.github.javafaker.Faker;
 import com.machineFactory.Model.*;
 import com.machineFactory.Repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class BackendProgrammeringExamApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendProgrammeringExamApplication.class, args);
+
+
     }
+        @Autowired
+        private Environment environment;
+
 
         Faker faker = new Faker();
         @Bean
@@ -35,60 +41,65 @@ public class BackendProgrammeringExamApplication {
                 ArrayList<Part> partsForMotor,
                 ArrayList<Part> partsForGearbox,
                 ArrayList<Part> partsForChain
+
             ) {
 
             return args -> {
+                    boolean env = Boolean.parseBoolean(environment.getProperty("BackendProgrammeringExam/src/main/resources/application.yml"));
 
-                //Faker that generates customers and addresses
-                for (int i = 0; i < 5; i++) {
-                    Customer customer = customerRepo.save(new Customer(faker.name().fullName(), faker.internet().emailAddress()));
-                    Address address = addressRepo.save(new Address(faker.address().streetAddress(), faker.address().zipCode(), faker.address().cityName()));
-                    customer.getAddresses().add(address);
-                    customerRepo.save(customer);
-                }
+                    if (env) {
+                        //Faker that generates customers and addresses
+                        for (int i = 0; i < 5; i++) {
+                            Customer customer = customerRepo.save(new Customer(faker.name().fullName(), faker.internet().emailAddress()));
+                            Address address = addressRepo.save(new Address(faker.address().streetAddress(), faker.address().zipCode(), faker.address().cityName()));
+                            customer.getAddresses().add(address);
+                            customerRepo.save(customer);
+                        }
 
-                Part pistonPart = new Part("Piston"); //Stempel
-                Part cylinderPart = new Part("Cylinder");
-                Part shaftPart = new Part("Shaft"); //aksel
-                Part sawtoothPart = new Part("Sawtooth"); //Sagtann
+                        Part pistonPart = new Part("Piston"); //Stempel
+                        Part cylinderPart = new Part("Cylinder");
+                        Part shaftPart = new Part("Shaft"); //aksel
+                        Part sawtoothPart = new Part("Sawtooth"); //Sagtann
 
-                partsForMotor.add(pistonPart);
-                partsForMotor.add(cylinderPart);
-                partsForGearbox.add(shaftPart);
-                partsForChain.add(sawtoothPart);
+                        partsForMotor.add(pistonPart);
+                        partsForMotor.add(cylinderPart);
+                        partsForGearbox.add(shaftPart);
+                        partsForChain.add(sawtoothPart);
 
-                Subassembly subassemblyMotor = new Subassembly("Motor", partsForMotor);
-                Subassembly subassemblyGearbox = new Subassembly("Gearbox", partsForGearbox);
-                Subassembly subassemblySaw = new Subassembly("Chain", partsForChain);
+                        Subassembly subassemblyMotor = new Subassembly("Motor", partsForMotor);
+                        Subassembly subassemblyGearbox = new Subassembly("Gearbox", partsForGearbox);
+                        Subassembly subassemblySaw = new Subassembly("Chain", partsForChain);
 
-                subassemblieListForTractor.add(subassemblyMotor);
-                subassemblieListForTractor.add(subassemblyGearbox);
+                        subassemblieListForTractor.add(subassemblyMotor);
+                        subassemblieListForTractor.add(subassemblyGearbox);
 
-                subassemblieListForChainSaw.add(subassemblySaw);
-                subassemblieListForChainSaw.add(subassemblyMotor);
+                        subassemblieListForChainSaw.add(subassemblySaw);
+                        subassemblieListForChainSaw.add(subassemblyMotor);
 
-                Machine machine1 = machineRepo.save(new Machine("Chainsaw", subassemblieListForChainSaw));
-                Machine machine2 = machineRepo.save(new Machine("Traktor", subassemblieListForTractor));
+                        Machine machine1 = machineRepo.save(new Machine("Chainsaw", subassemblieListForChainSaw));
+                        Machine machine2 = machineRepo.save(new Machine("Traktor", subassemblieListForTractor));
 
-                //Creates an order
-                Customer customer1 = new Customer("Jens");
-                customerRepo.save(customer1);
-                machines.add(machine1);
-                OrderModel order1 = orderRepo.save(new OrderModel(customer1, machines));
+                        //Creates an order
+                        Customer customer1 = new Customer("Jens");
+                        customerRepo.save(customer1);
+                        machines.add(machine1);
+                        OrderModel order1 = orderRepo.save(new OrderModel(customer1, machines));
 
-                //Create a customer, and add an address to it
-                Customer customer = customerRepo.save(new Customer("Helga", "helga@gmail.com"));
-                Address address = addressRepo.save(new Address("Løkkaveien 8", "8930", "Bergen"));
-                addresses.add(address);
-                customer.setAddresses(addresses);
-                customerRepo.save(customer);
+                        //Create a customer, and add an address to it
+                        Customer customer = customerRepo.save(new Customer("Helga", "helga@gmail.com"));
+                        Address address = addressRepo.save(new Address("Løkkaveien 8", "8930", "Bergen"));
+                        addresses.add(address);
+                        customer.setAddresses(addresses);
+                        customerRepo.save(customer);
 
-                //Create an address, and add it to a customer
-                Address address2 = addressRepo.save(new Address("Kyllingveien 3", "9483", "Tromsø"));
-                Customer customer2 = customerRepo.save(new Customer("Johny", "johny@gmail.com"));
-                customers.add(customer2);
-                address2.setCustomers(customers);
-                addressRepo.save(address2);
+                        //Create an address, and add it to a customer
+                        Address address2 = addressRepo.save(new Address("Kyllingveien 3", "9483", "Tromsø"));
+                        Customer customer2 = customerRepo.save(new Customer("Johny", "johny@gmail.com"));
+                        customers.add(customer2);
+                        address2.setCustomers(customers);
+                        addressRepo.save(address2);
+
+                    }
             };
         }
 }
