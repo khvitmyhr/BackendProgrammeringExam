f48a0ff728b76812a40d78d9c11d7d44e1ef7e48
khvitmyhr <kihv001@student.kristiania.no>
2023-12-15 16:39:25 +0100

    Lagt til test

---
+++ src/test/java/com/machineFactory/address/AddressUnitTest.java
@@ -0,0 +1,42 @@
+package com.machineFactory.address;
+
+import com.machineFactory.Model.Address;
+import com.machineFactory.Model.Customer;
+import com.machineFactory.Repository.AddressRepo;
+import com.machineFactory.Repository.CustomerRepo;
+import com.machineFactory.Service.AddressService;
+import com.machineFactory.Service.CustomerService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class AddressUnitTest {
+
+    @MockBean
+    AddressRepo addressRepo;
+
+    @Autowired
+    AddressService addressService;
+
+    //This test fails when you have test data in commandline runner, see more in ReadMe
+    @Test
+    public void shouldreturn1AddressFromMockedRepo() {
+
+        List<Address> addresses = List.of(new Address("Kyllingveien 3", "9999", "Hallingdal"));
+        when(addressRepo.findAll()).thenReturn(addresses);
+
+        List<Address> listOfAddresses = addressService.getAllAddresses();
+
+        assertThat(listOfAddresses.size()).isEqualTo(1);
+        assertThat(listOfAddresses.get(0).getStreet()).isEqualTo("Kyllingveien 3");
+        assertThat(listOfAddresses.get(0).getZipCode()).isEqualTo("9999");
+        assertThat(listOfAddresses.get(0).getCity()).isEqualTo("Hallingdal");
+    }
+}
