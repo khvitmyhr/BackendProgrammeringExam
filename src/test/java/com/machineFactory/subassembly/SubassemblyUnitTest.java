750939f333629a599018c86773f562048a0c3753
khvitmyhr <kihv001@student.kristiania.no>
2023-12-15 12:46:10 +0100

    rename

---
+++ src/test/java/com/machineFactory/subassembly/SubassemblyUnitTest.java
@@ -0,0 +1,44 @@
+package com.machineFactory.subassembly;
+
+import com.machineFactory.Model.Subassembly;
+import com.machineFactory.Repository.SubassemblyRepo;
+import com.machineFactory.Service.SubassemblyService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class SubassemblyUnitTest {
+
+    @MockBean
+    private SubassemblyRepo subassemblyRepo;
+
+    @Autowired
+    private SubassemblyService subassemblyService;
+
+    @Test
+    public void getSubassembies() {
+        List<Subassembly> subassemblies = List.of(new Subassembly());
+        when(subassemblyRepo.findAll()).thenReturn(subassemblies);
+
+        var subassemblies1 = subassemblyService.getAllSubassemblies();
+        assert subassemblies1.size() == 1;
+    }
+
+    @Test
+    public void getAllSubassemblies() {
+        List<Subassembly> subassemblies = List.of(new Subassembly("Tire"));
+        when(subassemblyRepo.findAll()).thenReturn(subassemblies);
+
+        List<Subassembly> listOfSubassemblies = subassemblyService.getAllSubassemblies();
+
+        assertThat(listOfSubassemblies.size()).isEqualTo(1);
+        assertThat(listOfSubassemblies.get(0).getSubassemblyName()).isEqualTo("Tire");
+    }
+}
